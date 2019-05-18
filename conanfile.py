087854cb7efef6d9993bf69fdabe6ab17d5ce95a
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-05-18 16:36:39 -0400

    Fixed conanfile path to point this project

--- conanfile.py
+++ conanfile.py
@@ -6,17 +6,17 @@ class CpprestsdkConan(ConanFile):
     version = "1.0.0"
     license = "MIT"
     author = "Nasim Kabiliravi <conanrepos@gmail.com>"
-    url = "https://github.com/conan-repos/cpprestsdk"
+    url = "https://github.com/mycpptutorial/helloworld-restapi-cpprestsdk-conan"
     description = "The C++ REST SDK is a Microsoft project for cloud-based client-server communication in native code using a modern asynchronous C++ API design. This project aims to help C++ developers connect to and interact with services."
-    topics = ("C++ REST SDK", "microservice", "restapi", "cpp")
+    topics = ("C++ REST SDK", "microservice", "restapi", "cpp", "C++ tutorial")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/conan-repos/cpprestsdk.git")
-        self.run("cd cpprestsdk && git checkout v2.10.13_conan && git submodule update --init")
+        self.run("git clone https://github.com/mycpptutorial/helloworld-restapi-cpprestsdk-conan.git")
+        self.run("cd helloworld-restapi-cpprestsdk-conan")
 
     def build(self):
         cmake = CMake(self)
@@ -30,7 +30,7 @@ class CpprestsdkConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
